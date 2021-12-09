cb482b9304f168c5c924776ecf3f462d63cbd93e
manjunadhb <manjunadhb@gmail.com>
2021-12-09 18:07:36 +0530

    adding screens

---
+++ src/screens/common/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div>
+            <button>Login as Student</button>
+            <button>Login as Admin</button>
+            <button>Sign Up</button>
+            <button>Forgot Password</button>
+        </div>
+    )
+}
