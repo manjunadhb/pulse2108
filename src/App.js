c4504fea7999513e08af7863b7235ed8ac104f1e
Santoshbellana <santoshbellana99@gmail.com>
2021-12-09 21:58:28 +0530

    Added Buttons In Landing Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import LandingPage from './screens/LandingPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          We can start working on this project.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <LandingPage/>
+
     </div>
   );
 }
---
+++ src/screens/LandingPage.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+export default function LandingPage() {
+    return (
+        <div>
+
+            <button>User Dashboard</button>
+            <button>Admin Dashboard</button>
+            <button>Forgot Password</button>
+            <button>Sign UP</button>
+            
+        </div>
+    )
+}
