41e507e9954e3de3d059a8f1fef83cc99d53c9ff
Bijinepalli Rajesh <brajesh9676@Achiever-23.local>
2021-12-11 20:32:08 +0530

    added tabs in managetasks

--- src/screens/admin/AdminDashBoard.js
+++ src/screens/admin/AdminDashBoard.js
@@ -1,8 +1,14 @@
 import React from 'react'
-import {Button,Container,Nav,Navbar,NavDropdown} from 'react-bootstrap'
+import {Button,Container,Nav,Navbar,NavDropdown,Tabs,Tab} from 'react-bootstrap'
 import {Link,Route,Routes,BrowserRoute} from 'react-router-dom'
 import pulseLogo from '../../images/brnlogopluse.png'
-
+import BatchSummary from './BatchSummary'
+import FeeReport from './FeeReport'
+import ManageTaskTab from './ManageTaskTab'
+import ManageUsers from './ManageUsers'
+import MessagesTab from './MessagesTab'
+import Ranking from './Ranking'
+import RequestsTab from './RequestsTab'
 export default function AdminDashBoard() {
     return (
         <div>
@@ -55,6 +61,44 @@ export default function AdminDashBoard() {
   </Navbar.Collapse>
   </Container>
 </Navbar>
+
+
+
+
+<Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+  <Tab eventKey="rankings" title="Rankings">
+
+   <Ranking/>
+    
+  </Tab>
+
+  <Tab eventKey="feereport" title="Fee Report">
+
+  <FeeReport/>
+
+  </Tab>
+
+  <Tab eventKey="Batchessummary" title="Batches Summary" >
+  <BatchSummary/>
+  </Tab>
+  <Tab eventKey="requests" title="Request" >
+  <RequestsTab/>
+  </Tab>
+
+  <Tab eventKey="messages" title="Messages" >
+  <MessagesTab/>
+</Tab>
+<Tab eventKey="manageTasks" title="Manage Tasks" >
+
+<ManageTaskTab/>
+
+</Tab>
+
+
+<Tab eventKey="manageusers" title="Manage Users" >
+<ManageUsers/>
+</Tab>
+</Tabs>
         </div>
     )
 }
