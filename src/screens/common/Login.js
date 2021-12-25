4fdf996d36ec6d71cb53f3f75adbfc44d4926e5d
Santoshbellana <santoshbellana99@gmail.com>
2021-12-25 17:23:08 +0530

    login screen update

--- src/screens/common/Login.js
+++ src/screens/common/Login.js
@@ -21,6 +21,63 @@ export default function Login() {
 
   return (
     <div className="mainContainer">
+      
+
+      
+
+
+    
+
+      <div className="galleryContainer ">
+        {/* <Carousel
+          variant="transparent"
+          fade
+          indicators
+          false
+          style={{
+            position: "relative",
+            bottom: "-350px",
+            textAlign: "center",
+          }}
+        >
+          <Carousel.Item>
+            <h3
+              style={{
+                color: "white",
+                fontFamily: "Dancing Script, cursive",
+              }}
+            >
+              "Success is not final; failure is not fatal: <br />
+              It is the courage to continue that counts."
+            </h3>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <h3
+              style={{
+                color: "white",
+                fontFamily: "Dancing Script, cursive",
+              }}
+            >
+              Coming together is a beginning; keeping together is progress;<br/>
+              working together is success.
+            </h3>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <h3
+              style={{
+                color: "white",
+                fontFamily: "Dancing Script, cursive",
+              }}
+            >
+              However difficult life may seem, <br/>there is always something you can
+              do and succeed at.
+            </h3>
+          </Carousel.Item>
+        </Carousel> */}
+      </div>
+
       <div className="loginContainer">
         <Container>
           <Row className="mt-5">
@@ -97,13 +154,16 @@ export default function Login() {
                 </div>
                 <br />
 
-                <Form.Group className="mb-3">
-                  <Nav.Link href="#home" style={{ color: "#6b09b5" }}  onClick={()=>{navigate("../common/SignUp") }} >
+                <Form.Group className="mb-5 text-center ">
+                  <Nav.Link href="#home" style={{ color: "#6b09b5"  }}  onClick={()=>{navigate("../common/SignUp") }} >
                  
-                    &nbsp; &nbsp; Not registered yet? Create an Account <br />
+                  Not registered yet? <br/> Create an Account 
                   </Nav.Link>
                   <Form.Group className="mb-3" id="copyRight">
-                    <h6 className="mt-5 p-5 text-center text-secondary ">
+                    <h6 className="mt-5  text-center  " style={{
+                                                                              position:'relative',
+                                                                              bottom:'-70px',
+                                                                              color:'#6b09b5'}}>
                       Copyright © 2021 BRN Infotech. All Rights Reserved.
                     </h6>
                   </Form.Group>
@@ -114,62 +174,8 @@ export default function Login() {
         </Container>
       </div>
 
-      {/* Content Div */}
-
-      {/* style={{
-            position:'relative',
-            bottom : '-350px'
-        }} */}
-
-      <div className="galleryContainer">
-        <Carousel
-          variant="transparent"
-          fade
-          indicators
-          false
-          style={{
-            position: "relative",
-            bottom: "-350px",
-            textAlign: "center",
-          }}
-        >
-          <Carousel.Item>
-            <h3
-              style={{
-                color: "white",
-                fontFamily: "Dancing Script, cursive",
-              }}
-            >
-              "Success is not final; failure is not fatal: <br />
-              It is the courage to continue that counts."
-            </h3>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <h3
-              style={{
-                color: "white",
-                fontFamily: "Dancing Script, cursive",
-              }}
-            >
-              Coming together is a beginning; keeping together is progress;<br/>
-              working together is success.
-            </h3>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <h3
-              style={{
-                color: "white",
-                fontFamily: "Dancing Script, cursive",
-              }}
-            >
-              However difficult life may seem, <br/>there is always something you can
-              do and succeed at.
-            </h3>
-          </Carousel.Item>
-        </Carousel>
-      </div>
+      
+      
     </div>
   );
 }
